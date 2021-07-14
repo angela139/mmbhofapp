bfcc6e119c56c00e2632ee6327541b757d7da67d
Angela Hu <angelahu925@gmail.com>
2021-07-13 22:54:07 -0700

    Created template for tour app

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Image, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.Header}>MMBHOF Catalog</Text>
+      <Image source={require('./assets/marin_logo.png')} style={{ height: 200, width: 250, resizeMode: 'contain' }} />
+      <View style={{ flexDirection: "row" }}>
+        <Image source={require('./assets/placeholder.jpg')} style={styles.Content} />
+        <Text style={styles.BodyHeader}>Bicycle Item</Text>
+      </View>
+      <Text style={styles.Body}>Random Description</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,6 +22,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  Header: {
+    color: '#fd6350',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginTop: 20
+  },
+  Content: {
+    height: 100,
+    width: 100,
+    resizeMode: 'contain',
+    marginRight: 30,
+  },
+  BodyHeader: {
+    color: '#000000',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginRight: 40
+  },
+  Body: {
+    color: '#000000',
+    fontSize: 14,
+    marginLeft: 90
+  }
 });
